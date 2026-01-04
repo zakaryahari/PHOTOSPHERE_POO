d83daae799024bfd384be0bcd58e14ad1f763135
zakarya hari <zakariahari42@gmail.com>
2026-01-04 21:46:24 +0100

    Creation de class Database.php qui'il va avoir la connection vere la database.

---
+++ POO/database.php
@@ -0,0 +1,34 @@
+<?php 
+class Database {
+    private $host = "localhost";
+    private $db_name = "photosphere";
+    private $username = "root";
+    private $password = "";
+    private $port = "3307";
+    private static $instance = null;
+    private $conn;
+
+
+    private function __construct() {
+        try {
+            $dsn = "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name;
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+            
+        } catch(PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
