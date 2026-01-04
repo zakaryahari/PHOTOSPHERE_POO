329d4871841bfc5dc6d5c05ba5c8baaf3f5455bb
zakarya hari <zakariahari42@gmail.com>
2026-01-04 23:27:58 +0100

    Creation des methode findbyemail qui a recupiry l'utilisateur par son email , et aussi la methode getextradata.

--- POO/Repositorys.php
+++ POO/Repositorys.php
@@ -10,6 +10,46 @@ class UserRepository implements UserRepositoryInterface {
         $this->db = $conn;
     }
 
+    public function findByEmail(string $email) : User {
+        $sql = "select * from User where email = :email";
+
+        $query = $this->db->getConnection()->prepare($sql);
+        $query->bindParam(":email" , $email);
+
+        if($query->execute()) {
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+            if (!$row) return null;
+
+            $id = $row['id_user'];
+
+            if ($row['role'] === 'admin') {
+                $extra = $this->getExtraData('Admin', $id);
+                return new Admin(array_merge($row, $extra));
+            } 
+            
+            if ($row['role'] === 'pro') {
+                $extra = $this->getExtraData('Pro_User', $id);
+                return new ProUser(array_merge($row, $extra));
+            }
+
+            if ($row['role'] === 'basic') {
+                $extra = $this->getExtraData('Basic_User', $id);
+                return new BasicUser(array_merge($row, $extra));
+            }
+
+
+            return null;
+        }
+        return false;
+    }
+
+    public function getExtraData(string $table , int $id) {
+        $sql = "select * from $table where id_user = :id" ;
+        $row = $this->db->getConnection()->prepare($sql);
+        $row->bindParam(":id" , $id);
+        $row->execute();
+        return $row->fetch(PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
