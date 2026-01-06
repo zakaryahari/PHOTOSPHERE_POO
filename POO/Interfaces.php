821d8c96c24fd04c1f5e3e2c46ace0eea2d070d3
zakarya hari <zakariahari42@gmail.com>
2026-01-06 12:50:13 +0100

    Creation de l'interface Likeable avec la signateur de ses methodes .

--- POO/Interfaces.php
+++ POO/Interfaces.php
@@ -81,5 +81,17 @@ interface Commentable {
     public function getCommentCount(): int;
 }
 
+interface Likeable {
+
+    public function addLike(int $userId): bool;
+
+    public function removeLike(int $userId): bool;
+
+    public function isLikedBy(int $userId): bool;
+
+    public function getLikeCount(): int;
+    
+    public function getLikedBy(): array;
+}
 
 ?>
