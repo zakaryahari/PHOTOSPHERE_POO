fc047a9eac767809a0a816eae720146a5683dee8
zakarya hari <zakariahari42@gmail.com>
2026-01-07 08:58:21 +0100

    Creation de Trait CommentableTrait avec ses methodes .

--- POO/Trait.php
+++ POO/Trait.php
@@ -87,4 +87,30 @@ trait LikeableTrait {
     }
 }
 
+trait CommentableTrait {
+    protected array $comments = [];
+    protected int $commentCount = 0;
+
+    public function addComment(string $content, int $userId): int {
+        $this->comments[] = ['user' => $userId, 'text' => $content];
+        $this->commentCount++;
+        return count($this->comments);
+    }
+
+    public function getComments(): array {
+        return $this->comments;
+    }
+
+    public function getCommentCount(): int {
+        return $this->commentCount;
+    }
+
+    public function removeComment(int $commentId): bool {
+        if ($this->commentCount > 0) {
+            $this->commentCount--;
+            return true;
+        }
+        return false;
+    }
+}
 ?>
